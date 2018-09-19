5d44c1776ea5069b4639cacc751cea9ee6a655c6
YaotingJiang <yaotingjiang@gmail.com>
2018-09-19 19:10:36 -0400

    work complete

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ "x$PROD" == "x" ]]; then 
-	echo "This script is for starting in production."
-	echo "Use"
-	echo "   mix phx.server"
-	exit
-fi
+# if [[ "x$PROD" == "x" ]]; then
+# 	echo "This script is for starting in production."
+# 	echo "Use"
+# 	echo "   mix phx.server"
+# 	exit
+# fi
 
 # TODO: Enable this script by removing the above.
 
@@ -18,10 +18,9 @@ _build/prod/rel/practice/bin/practice stop || true
 
 echo "Starting app..."
 
-_build/prod/rel/practice/bin/practice foreground
+_build/prod/rel/practice/bin/practice start
 
 # TODO: Change "foreground" to "Start"
 
 # TODO: Add a cron rule or systemd service file
 #       to start your app on system boot.
-
